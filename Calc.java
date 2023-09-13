c9598a9bf337b709bc5111138936a1d277fc9d54
Dovgal Andrey <a.nic.dovgal@gmail.com>
2023-09-13 17:45:05 +0300

    new

--- Calc.java
+++ Calc.java
@@ -7,6 +7,7 @@ String s1 = new String("Java-01");
 String s2 = "Java-02";
 
 System.out.println(s1.charAt(0));
+System.out.println(s1.codePointAt(0));
 //System.out.println(s1.replace(3, 0));
 System.out.println(s2.charAt(2));
 
