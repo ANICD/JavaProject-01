1898a7eea7097cedaffee3788d6da8b5ea8a3754
Dovgal Andrey <a.nic.dovgal@gmail.com>
2023-09-13 17:38:42 +0300

    new

---
+++ Calc.java
@@ -0,0 +1,14 @@
+import javax.lang.model.util.Elements.Origin;
+
+public class Calc {
+public static void main (String[] args){
+//String s1 = "Java-01";   
+String s1 = new String("Java-01");
+String s2 = "Java-02";
+
+System.out.println(s1.charAt(0));
+//System.out.println(s1.replace(3, 0));
+System.out.println(s2.charAt(2));
+
+}
+}
