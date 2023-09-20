6fc8cf45dce3f164bcf84b8eabf5fb6b61c04920
Dovgal Andrey <a.nic.dovgal@gmail.com>
2023-09-21 00:58:29 +0300

    21-09-2023|01:00

--- Calc.java
+++ Calc.java
@@ -1,10 +1,7 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,8 +12,10 @@ public class Calc {
             String inputData = "";
             inputData = in.nextLine();
             inputData = inputData.toUpperCase();
-            int arg01, arg02, res;
-
+            int arg01, arg02;
+            // if ((inputData.contains("I") || inputData.contains("V") ||
+            // inputData.contains("X") && inputList.contains(regex:"[1-10]")))
+            // throw new Exception("Оба числа должны быть или римские, или арабские");
             // Обработка римских чисел
             if (inputData.contains("I") || inputData.contains("V") || inputData.contains("X")) {
                 System.out.println("Работаем с римскими числами!");
@@ -32,28 +31,19 @@ public class Calc {
                 // (их должно быть не более двух)
                 if (romansList.size() != 2)
                     throw new Exception("Небходимо ввести два аргумента!");
-                // Получаем первое число из списка romansList и помещаем его в символьный массив
-                // char[] arg01CharArray = romansList.get(0).toCharArray();
-                // arg01 = numConverter(arg01CharArray); // Применяем к массиву функцию
-                arg01 = numConverter(romansList, 0);
-                arg02 = numConverter(romansList, 1);
-                System.out.println(arg01 + " " + arg02);
-                // преобразования в арабское число
-                // Получаем второе число из списка romansList и помещаем его в символьный массив
-                // char[] arg02CharArray = romansList.get(1).toCharArray();
-                // arg02 = numConverter(arg01CharArray); // Применяем к массиву функцию
-                // char[] output01 = numConverter(romansList, 0);
-                // char[] output02 = numConverter(romansList, 1);
-                //System.out.println(romansList);
-                //System.out.println(romansList.get(0) + " " + romansList.get(1));
-                // преобразования в арабское число
-                // Дальше можно работать как с арабскими числами
-                // System.out.println(output01[0] + output01[1] + " " + output02[0] +
-                // output02[1]);
-                // System.out.println(arg01CharArray[0] + " " + arg01CharArray[1]);
-
+                arg01 = RomansConverter(romansList, 0);// Ищем первое число с помощью конвертера
+                arg02 = RomansConverter(romansList, 1);// Ищем второе число с помощью конвертера
+                if ((arg01 > 10) && (arg02 > 10))
+                    throw new Exception("Неправильно введены оба числа! Числа должны быть от I до X");
+                if (arg01 > 10)
+                    throw new Exception("Неправильно введено первое число! Число должно быть от I до X");
+                if (arg02 > 10)
+                    throw new Exception("Неправильно введено второе число! Число должно быть от I до X");
+                CalcRomansNumber(arg01, arg02, inputData, romansList); // Применяем процедуру вычисления
+                // System.out.println(arg01 + " " + arg02);
             } else {
                 // Обработка арабских чисел
+                System.out.println("Работаем с арабскими числами!");
                 List<String> arabiansList = new ArrayList<String>();
                 Pattern p02 = Pattern.compile("\\d+");
                 Matcher m02 = p02.matcher(inputData);
@@ -70,50 +60,66 @@ public class Calc {
                     throw new Exception("Неправильно введено первое число! Число должно быть от 1 до 10");
                 if (arg02 > 10)
                     throw new Exception("Неправильно введено второе число! Число должно быть от 1 до 10");
-                System.out.println("Работаем с арабскими числами!");
-                res = calculator(arg01, arg02, inputData);
-                System.out.println(res);
+                CalcArabNumber(arg01, arg02, inputData);
             }
-        } catch (
-
-        Exception a) {
+        } catch (Exception a) {
             a.printStackTrace();
         }
-
     }
 
     // ФУНКЦИЯ КОНВЕРТАЦИИ РИМСКИХ ЧИСЕЛ В АРАБСКИЕ
-    public static int numConverter(List<String> inputString, int index) {
-        int res = 0;
-        char[] inputStringArray = inputString.get(index).toCharArray();
-        //System.out.println(inputStringArray);
-        //System.out.println("Длина массива: " + inputStringArray.length);
-        // char valueOfEndSymbol = fromStrinIg[fromStrinIg.length - 1];
+    public static int RomansConverter(List<String> inputString, int index) {
+        char[] inputStringArray = inputString.get(index).toCharArray(); // Преобразование List в массив
         // Создаем МАР
-        HashMap<Character, Integer> romanMap = new HashMap<>();{
-        romanMap.put('I', 1);
-        romanMap.put('V', 5);
-        romanMap.put('X', 10);
-        romanMap.put('L', 50);
-        romanMap.put('C', 100);
-        romanMap.put('D', 500);
-        romanMap.put('M', 1000);
+        HashMap<Character, Integer> romanMap = new HashMap<Character, Integer>();
+        {
+            romanMap.put('I', 1);
+            romanMap.put('V', 5);
+            romanMap.put('X', 10);
+            romanMap.put('L', 50);
+            romanMap.put('C', 100);
+            romanMap.put('D', 500);
+            romanMap.put('M', 1000);
         }
-        if (inputString.size() == 1) {
-            char valueOfEndSymbol = inputStringArray[inputStringArray.length-1];
-            res = romanMap.get(valueOfEndSymbol);
+        int res = romanMap.get(inputStringArray[inputStringArray.length - 1]);
+        if (inputStringArray.length == 1)
+            res = romanMap.get(inputStringArray[inputStringArray.length - 1]);
+        else {
+            for (int n = inputStringArray.length - 2; n >= 0; n--) {
+                int currSymbol = romanMap.get(inputStringArray[n]);
+                int prevSymbol = romanMap.get(inputStringArray[n + 1]);
+                if (currSymbol < prevSymbol) {
+                    res -= currSymbol;
+                } else {
+                    res += currSymbol;
+                }
+            }
         }
-         else {
-res = 2;
-         }
         return res;
     }
 
-    public static int calculator(int arg1, int arg2, String input) {
+    public static void CalcRomansNumber(int arg1, int arg2, String input, List<String> inputList) {
         int res = 0;
         if (input.contains("+")) { // Делаем сложение
             res = arg1 + arg2;
-            System.out.println("Сумма чисел " + arg1 + " и " + arg2 + " равна " + res);
+            System.out.println("Сумма чисел " + inputList.get(0) + " и " + inputList.get(1) + " равна " + res);
+        } else if (input.contains("-")) { // Делаем вычитание
+            res = arg1 - arg2;
+            System.out.println("Разность чисел " + inputList.get(0) + " и " + inputList.get(1) + " равна " + res);
+        } else if (input.contains("*")) { // Делаем умножение
+            res = arg1 * arg2;
+            System.out.println("Произведение чисел " + inputList.get(0) + " и " + inputList.get(1) + " равна " + res);
+        } else if (input.contains("/")) { // Делаем деление
+            res = arg1 / arg2;
+            System.out.println("Частное чисел " + inputList.get(0) + " и " + inputList.get(1) + " равно " + res);
+        }
+    }
+
+    public static void CalcArabNumber(int arg1, int arg2, String input) {
+        int res = 0;
+        if (input.contains("+")) { // Делаем сложение
+            res = arg1 + arg2;
+            System.out.println("Сумма чисел " + input + " и " + arg2 + " равна " + res);
         } else if (input.contains("-")) { // Делаем вычитание
             res = arg1 - arg2;
             System.out.println("Разность чисел " + arg1 + " и " + arg2 + " равна " + res);
@@ -124,6 +130,5 @@ res = 2;
             res = arg1 / arg2;
             System.out.println("Частное чисел " + arg1 + " и " + arg2 + " равно " + res);
         }
-        return res;
     }
 }
